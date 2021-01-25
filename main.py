7e7ec7c56606ac1d1fa64f590382ff91542032da
th3end0f3v4ng3l10n <th3end0f3v4ng3l10n@gmail.com>
2021-01-25 23:35:56 +0500

    V2

---
+++ main.py
@@ -0,0 +1,65 @@
+#Импортирование библиотек
+import smtplib #smtp
+from email.mime.multipart import MIMEMultipart #Это для сообщений
+from email.mime.text import MIMEText #Это тоже для сообщений
+from time import sleep #для контроля времени
+import configparser #Чтобы читать конфиг
+from colorama import Fore, Style #КРАСОТА
+
+class Main():
+    def __init__(self):
+        '''Инициализация'''
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini') #Чтение config.ini
+
+        self.login = self.config['MY_DATA']['login'] #берем логин из config.ini
+        self.password = self.config['MY_DATA']['password'] #берем пароль из config.ini
+
+        self.message = self.config['Message']['msg'] #Берем сообещние из config.ini
+        self.subject = self.config['Message']['subject'] #Берем TITLE Из config.ini
+
+        self.emails = []
+
+        with open('emails') as f:
+            for line in f:
+                self.emails.append(line.replace('\n',''))
+
+
+    def create_yandex_database(self):
+        '''Создаёт базу из yandex доменов'''
+        f = open('yandex_database.txt','w')
+        database_yandex = []
+        f.truncate()
+        for i in self.emails:
+            if '@yandex' in i:
+                f.write(i+'\n')
+
+            else:
+                pass
+
+
+        f.close()
+
+    def function(self):
+        '''Функция отправляет сообщения'''
+        def krasota(email):
+            '''Самая красивая функция....'''
+            print('[',Fore.GREEN +'+', Style.RESET_ALL+']'," Успешно отправлено на почту... {}".format(email))
+
+        for email in self.emails: #для каждого email в списке self.emails
+            msg = MIMEMultipart()
+            msg['From'] = self.login
+            msg['To'] = email # ну тут понятно. Сюда тыкаем email на которой отправляем, но у нас всё в цикле for, который берет email из списка
+            msg['Subject'] = self.subject #TITLE
+
+            msg.attach(MIMEText(self.message, 'plain')) #делаем сообещние
+            server = smtplib.SMTP('smtp.gmail.com: 587') #smtp
+            server.starttls()   #стартуем tls
+            server.login(msg['From'], self.password) # логинимся в smtp
+            server.sendmail(msg['From'], msg['To'], msg.as_string()) #отправляем сообщение
+            server.quit() #kill server :)
+            krasota(email) # название говорит само за себя
+
+root = Main()
+root.function()
+root.create_yandex_database()
