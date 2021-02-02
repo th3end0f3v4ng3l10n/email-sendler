7c3df17a70cdbd8b03c8f546a361b40b678c2011
th3end0f3v4ng3l10n <th3end0f3v4ng3l10n@gmail.com>
2021-02-02 16:44:02 +0500

    add gui

--- main.py
+++ main.py
@@ -7,28 +7,12 @@ import configparser #Чтобы читать конфиг
 from colorama import Fore, Style #КРАСОТА
 import PySimpleGUI as sg
 from tkinter import *
-class Interface():
-    def main_window(self):
-        layout = [
-                [sg.Text('Адреса: '), sg.InputText(), sg.FileBrowse()],
-                [sg.Output(key='-OUT-', size=(80, 20))],
-                [sg.Button('Sumbit'), sg.Cancel(), sg.Button('Exit')]]
-        window = sg.Window('Game Finder', layout, element_justification='center').finalize()
-        window['-OUT-'].TKOut.output.config(wrap='word') # set Output element word wrapping
+import selenium
+from selenium import webdriver
+import geckodriver_autoinstaller
 
 
-        while True:
-            event, values = window.read()
-            if event == sg.WIN_CLOSED:
-                break
-                window.close()
-            elif event == 'Sumbit':
-                root = Main()
-                root.create_yandex_database()
-                root.function()
-            elif event == 'Exit':
-                break
-        window.close()
+
 
 class Main():
     def __init__(self):
@@ -39,54 +23,91 @@ class Main():
         self.login = self.config['MY_DATA']['login'] #берем логин из config.ini
         self.password = self.config['MY_DATA']['password'] #берем пароль из config.ini
 
-        self.message = self.config['Message']['msg'] #Берем сообещние из config.ini
+        self.message = []
         self.subject = self.config['Message']['subject'] #Берем TITLE Из config.ini
 
         self.emails = []
 
-        with open('emails') as f:
-            for line in f:
-                self.emails.append(line.replace('\n',''))
 
 
-    def create_yandex_database(self):
-        '''Создаёт базу из yandex доменов'''
-        f = open('yandex_database.txt','w')
-        database_yandex = []
-        f.truncate()
-        for i in self.emails:
-            if '@yandex' in i:
-                f.write(i+'\n')
+    def create_window(self):
+        layout = [
+                [sg.Text('Адреса: '), sg.InputText(), sg.FileBrowse(key = '-IN-')],
+                [sg.Text('Сообщение: '), sg.FileBrowse(key = '-key-')],
+                [sg.Output(key='-OUT-', size=(80, 20))],
+                [sg.Button('Look'),sg.Button('Sumbit'), sg.Cancel(), sg.Button('Exit')]]
+        window = sg.Window('Email Sendler', layout, element_justification='center').finalize()
+        window['-OUT-'].TKOut.output.config(wrap='word') # set Output element word wrapping
 
-            else:
-                pass
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED:
+                break
+                window.close()
+
+            elif event == 'Sumbit':
+                with open(values['-key-'], 'r') as f:
+                    for line in f:
+                        self.message.append(line)
+
+                self.message_string = ' '.join(self.message)
+
+                print('Файл с адресами указан: ', values['-IN-'])
+                with open(values['-IN-'], 'r') as f:
+                    for line in f:
+                        print(line.replace('\n', ''))
+                        self.emails.append(line.replace('\n', ''))
+                print('Все адреса успешно добавлены =)')
+
+                print('Делаю базу яндекс адресов...')
+                f = open('yandex_database.txt','w')
+                database_yandex = []
+                f.truncate()
+                for i in self.emails:
+                    if '@yandex' in i:
+                        f.write(i+'\n')
+                    else:
+                        pass
+
+                print('')
+                print('[ + ] Начинаю отправлять сообщения...')
+
+                for email in self.emails:
+                    msg = MIMEMultipart()
+                    msg['From'] = self.login
+                    msg['To'] = email # ну тут понятно. Сюда тыкаем email на которой отправляем, но у нас всё в цикле for, который берет email из списка
+                    msg['Subject'] = self.subject #TITLE
+
+                    msg.attach(MIMEText(self.message_string, 'html')) #делаем сообещние
+                    server = smtplib.SMTP('smtp.gmail.com: 587') #smtp
+                    server.starttls()   #стартуем tls
+                    server.login(msg['From'], self.password) # логинимся в smtp
+                    server.sendmail(msg['From'], msg['To'], msg.as_string()) #отправляем сообщение
+                    server.quit() #kill server :)
+                    print('Успешно отправлено на адрес.. ', email) # название говорит само за себя
+                print('<=====Успешно завершено, хорошей работы=====>')
 
 
-        f.close()
+            elif event == 'Look':
+                try:
+                    geckodriver_autoinstaller.install()
+                except:
+                    print("Драйвер не установлен. Свяжитесь с кодером или с ")
+                print('Message Preview')
+                driver = webdriver.Chrome()
+                driver.get('file://'+values['-key-'])
 
-    def function(self):
-        '''Функция отправляет сообщения'''
-        def krasota(email):
-            '''Самая красивая функция....'''
-            print('[',Fore.GREEN +'+', Style.RESET_ALL+']'," Успешно отправлено на почту... {}".format(email))
+            elif event == 'Exit':
+                break
+        window.close()
 
-        for email in self.emails: #для каждого email в списке self.emails
-            msg = MIMEMultipart()
-            msg['From'] = self.login
-            msg['To'] = email # ну тут понятно. Сюда тыкаем email на которой отправляем, но у нас всё в цикле for, который берет email из списка
-            msg['Subject'] = self.subject #TITLE
 
-            msg.attach(MIMEText(self.message, 'plain')) #делаем сообещние
-            server = smtplib.SMTP('smtp.gmail.com: 587') #smtp
-            server.starttls()   #стартуем tls
-            server.login(msg['From'], self.password) # логинимся в smtp
-            server.sendmail(msg['From'], msg['To'], msg.as_string()) #отправляем сообщение
-            server.quit() #kill server :)
-            krasota(email) # название говорит само за себя
+
+
+
 def use_all():
-    #root = Main()
+    root = Main()
+    root.create_window()
+
     #root.function()
-    #root.create_yandex_database()
-    interface = Interface()
-    interface.main_window()
 use_all()
